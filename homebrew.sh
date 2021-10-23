44461b32cfc2f84d33310865bfca469d512f2408
Zachary Rideout <40773197+zrides@users.noreply.github.com>
2021-10-23 16:31:10 -0400

    Made a list of the homebrew I use

--- homebrew.sh
+++ homebrew.sh
@@ -1,116 +1,15 @@
-aom
-bdw-gc
-c-ares
-cairo
---> cask
-coreutils
-dav1d
-emacs
---> ffmpeg
-flac
-fontconfig
-freetype
-frei0r
-fribidi
-gdbm
-gettext
-giflib
---> git
-glib
-gmp
-gnupg
-gnutls
-gobject-introspection
-graphite2
-guile
-harfbuzz
---> htop
-icu4c
-jansson
-jemalloc
-jpeg
-lame
-leptonica
-libass
-libassuan
-libbluray
-libev
-libevent
-libffi
-libgcrypt
-libgpg-error
-libidn2
-libksba
-libogg
-libpng
-libpthread-stubs
-libsamplerate
-libsndfile
-libsodium
-libsoxr
-libtasn1
-libtiff
-libtool
-libunistring
-libusb
-libvidstab
-libvorbis
-libvpx
-libx11
-libxau
-libxcb
-libxdmcp
-libxext
-libxrender
-little-cms2
-lzo
-m4
-macprefs
---> md5deep
-mpdecimal
-ncurses
-nettle
-nghttp2
---> nmap
-npth
-opencore-amr
-openjpeg
-openssl@1.1
-opus
-p11-kit
-pcre
-pcre2
-pinentry
-pixman
-pkg-config
---> powerlevel10k
-python@3.9
-rav1e
-readline
-rubberband
-sdl2
-snappy
-speex
-sqlite
-srt
-tesseract
-theora
---> tmux
-unbound
-utf8proc
-webp
---> wget
-x264
-x265
-xorgproto
-xvid
-xz
---> youtube-dl
-zeromq
-zimg
-zsh
-zsh-history-substring-search
--------------------------- CASKS -----------------------------
+
+brew install cask
+brew install ffmpeg
+brew install git
+brew install htop
+brew install md5deep
+brew install nmap
+brew install powerlevel10k
+brew install tmux
+brew install wget
+brew install youtube-dl
+# -------------------------- CASKS -----------------------------
 brew install --cask alfred
 brew install --cask iina
 brew install --cask iterm2
