b73cf29982813fa128937102fdb2e903c67ef247
Zachary Rideout <40773197+zrides@users.noreply.github.com>
2021-10-23 16:30:41 -0400

    Made a list of the homebrew I use

---
+++ homebrew.sh
@@ -0,0 +1,134 @@
+aom
+bdw-gc
+c-ares
+cairo
+--> cask
+coreutils
+dav1d
+emacs
+--> ffmpeg
+flac
+fontconfig
+freetype
+frei0r
+fribidi
+gdbm
+gettext
+giflib
+--> git
+glib
+gmp
+gnupg
+gnutls
+gobject-introspection
+graphite2
+guile
+harfbuzz
+--> htop
+icu4c
+jansson
+jemalloc
+jpeg
+lame
+leptonica
+libass
+libassuan
+libbluray
+libev
+libevent
+libffi
+libgcrypt
+libgpg-error
+libidn2
+libksba
+libogg
+libpng
+libpthread-stubs
+libsamplerate
+libsndfile
+libsodium
+libsoxr
+libtasn1
+libtiff
+libtool
+libunistring
+libusb
+libvidstab
+libvorbis
+libvpx
+libx11
+libxau
+libxcb
+libxdmcp
+libxext
+libxrender
+little-cms2
+lzo
+m4
+macprefs
+--> md5deep
+mpdecimal
+ncurses
+nettle
+nghttp2
+--> nmap
+npth
+opencore-amr
+openjpeg
+openssl@1.1
+opus
+p11-kit
+pcre
+pcre2
+pinentry
+pixman
+pkg-config
+--> powerlevel10k
+python@3.9
+rav1e
+readline
+rubberband
+sdl2
+snappy
+speex
+sqlite
+srt
+tesseract
+theora
+--> tmux
+unbound
+utf8proc
+webp
+--> wget
+x264
+x265
+xorgproto
+xvid
+xz
+--> youtube-dl
+zeromq
+zimg
+zsh
+zsh-history-substring-search
+-------------------------- CASKS -----------------------------
+brew install --cask alfred
+brew install --cask iina
+brew install --cask iterm2
+brew install --cask jettison
+brew install --cask spotify
+brew install --cask steam 
+brew install --cask visual-studio-code
+brew install --cask google-chrome
+brew install --cask appcleaner
+brew install --cask arduino
+brew install --cask bartender
+brew install --cask microsoft-remote-desktop
+brew install --cask prusaslicer
+brew install --cask private-internet-access
+brew install --cask transmission
+brew install --cask transmit
+brew install --cask unity
+
+
+
+
